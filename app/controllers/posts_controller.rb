139117c96eb43cd3842306da3f7ec9caf2580162
mserino <m.serino@hotmail.it>
2014-10-28 15:55:05 +0000

    Can edit a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,17 @@ class PostsController < ApplicationController
 		redirect_to posts_path
 	end
 
+	def edit
+		@post = Post.find params[:id]
+	end
+
+	def update
+		@post = Post.find params[:id]
+		@post.update post_params
+		@post.save
+		redirect_to posts_path
+	end
+
 	def post_params
 		params.require(:post).permit(:title, :description)		
 	end
---
+++ spec/features/editing_posts_feature_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'Editing posts' do
+	before do
+		Post.destroy_all
+		@post = Post.create title: 'Cool post', description: 'Wow I am so cool'
+	end
+
+	it 'can edit a post' do
+		visit posts_path
+		click_link 'Edit post'
+		fill_in 'Title', with: 'Changing the post'
+		click_button 'Submit'
+		expect(current_path).to eq posts_path
+		expect(page).to have_content 'Changing the post'
+		expect(page).not_to have_content 'Cool post'
+	end
+end
