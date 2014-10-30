adec11e05bbbee3d0ae05f2290c4b20646ac674c
mserino <m.serino@hotmail.it>
2014-10-30 17:49:50 +0000

    Added comments on a new page, tested

--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -15,12 +15,23 @@ describe 'Comments' do
 
 	it 'can add one comment' do
 		visit root_path
-		within('.comment') do
-			fill_in "Name", with: "Sue"
-			fill_in "Email", with: "sue@test.com"
-			fill_in "Body", with: "This post is very cool"
-			click_button 'Submit'
-		end
+		click_link "Leave a comment"
+		leave_comment
+		expect(current_path).to eq posts_path
 		expect(page).to have_content "1 comments"
+	end
+
+	it 'can show the comment' do
+		visit root_path
+		click_link "Leave a comment"
+		leave_comment
+		expect(page).to have_content "This post is very cool"
+	end
+
+	def leave_comment
+		fill_in "Name", with: "Sue"
+		fill_in "Email", with: "sue@test.com"
+		fill_in "Leave your comment", with: "This post is very cool"
+		click_button 'Submit'
 	end
 end
