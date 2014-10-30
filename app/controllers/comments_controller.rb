6a12359d475a4bf1740a88b66f47c554b2850818
mserino <m.serino@hotmail.it>
2014-10-30 18:38:41 +0000

    Can edit or delete comment only if owner of the post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,33 @@ class CommentsController < ApplicationController
 
 	def create
 		@post = Post.find params[:post_id]
-		@comment = @post.comments.create params[:comment].permit(:name, :email, :body)
+		@comment = @post.comments.create comment_params
+		flash[:notice] = "Thank you for your comment"
 		redirect_to posts_path
+	end
+
+	def edit
+		@post = Post.find params[:post_id]
+		@comment = Comment.find params[:id]
+	end
+
+	def update
+		@comment = Comment.find params[:id]
+		@comment.update comment_params
+		@comment.save
+		flash[:notice] = "Comment successfully updated"
+		redirect_to posts_path
+	end
+
+	def destroy
+		@comment = Comment.find params[:id]
+		@comment.destroy
+		flash[:notice] = "Comment successfully deleted"
+		redirect_to posts_path
+	end
+
+
+	def comment_params
+		params.require(:comment).permit(:name, :email, :body)
 	end
 end
--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -3,32 +3,73 @@ require 'rails_helper'
 describe 'Comments' do
 	let(:jack) {create(:jack)}
 
-	before do
-		login_as jack
-		@post = jack.posts.create title: "Hello I am Jack", description: "And this is my post"
+	context 'logged in' do
+		before do
+			login_as jack
+			@post = jack.posts.create title: "Hello I am Jack", description: "And this is my post"
+		end
+
+		context 'no comments' do
+			it 'has no comments' do
+				visit root_path
+				expect(page).to have_content "0 comments"
+			end
+		end
+
+		context 'with comments' do
+			before do
+				leave_comment
+			end
+
+			it 'can add one comment' do
+				expect(current_path).to eq posts_path
+				expect(page).to have_content "1 comments"
+			end
+
+			it 'can show the comment' do
+				expect(page).to have_content "This post is very cool"
+			end
+
+			it 'can edit a comment if logged in' do
+				click_link "Edit this comment"
+				fill_in "Name", with: "Anna"
+				click_button "Submit"
+				expect(page).not_to have_content "Sue"
+				expect(page).to have_content "Anna"
+				expect(page).to have_content "Comment successfully updated"
+			end
+
+			it 'can delete a comment if logged in' do
+				click_link "Delete this comment"
+				expect(page).not_to have_content "Sue"
+				expect(page).to have_content "0 comments"
+				expect(page).to have_content "Comment successfully deleted"
+			end
+		end
 	end
 
-	it 'has no comments' do
-		visit root_path
-		expect(page).to have_content "0 comments"
-	end
+	context 'logged out' do
+		before do
+			login_as jack
+			@post = jack.posts.create title: "Hello I am Jack", description: "And this is my post"
+			leave_comment
+			click_link "Sign out"
+		end
 
-	it 'can add one comment' do
-		visit root_path
-		click_link "Leave a comment"
-		leave_comment
-		expect(current_path).to eq posts_path
-		expect(page).to have_content "1 comments"
-	end
+		it 'cannot edit comment' do
+			visit root_path
+			expect(page).not_to have_content 'Edit this comment'
+		end
 
-	it 'can show the comment' do
-		visit root_path
-		click_link "Leave a comment"
-		leave_comment
-		expect(page).to have_content "This post is very cool"
+		it 'cannod delete comment' do
+			visit root_path
+			expect(page).not_to have_content 'Delete this comment'
+		end
 	end
 
 	def leave_comment
+		visit root_path
+		click_link "Leave a comment"
 		fill_in "Name", with: "Sue"
 		fill_in "Email", with: "sue@test.com"
 		fill_in "Leave your comment", with: "This post is very cool"
